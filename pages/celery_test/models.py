3364e13371ba1dc11bd6b6b547be3fe4ac58f15a
Dave Masog <dave@masog.com>
2014-11-17 04:06:17 +0000

    collectstatic works on S3

--- pages/celery_test/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class CounterModel(models.Model):
-    count = models.IntegerField(default=0)
-
-    def __unicode__(self):
-        return unicode(self.count)
--- pages/weather/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from weather.models import Reading
-
-admin.site.register(Reading)
