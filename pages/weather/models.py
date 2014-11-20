f5d08fef797dc2f12c537c670137ca752c34f5f6
Dave Masog <dave@masog.com>
2014-11-20 00:13:46 +0000

    fixed weather model record creation time

--- pages/weather/models.py
+++ pages/weather/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Reading(models.Model):
     slug = models.SlugField(max_length=40,default="raleigh")
     d  = models.DateField(default=datetime.now())
-    dt = models.DateTimeField(default=datetime.now())
+    dt = models.DateTimeField(default=datetime.now)
     location = models.CharField(max_length=200)
     wind = models.CharField(max_length=75)
     sky_conditions = models.CharField(max_length=30)
