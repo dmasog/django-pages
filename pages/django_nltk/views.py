ff93f8fe7788a9083be34dbd527c4549bd0d65f1
Dave Masog <dave@masog.com>
2014-11-21 19:36:35 +0000

    nltk returns word definitions

--- pages/django_nltk/views.py
+++ pages/django_nltk/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 import datetime
+import nltk
+from nltk.corpus import wordnet as wn
+import enchant
 
-def home(request):
+def home(request,parm=""):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    resp = "%s:<br><ol>" % parm 
+    for i in wn.synsets(parm):
+        resp += "<li>%s</li>" % i.definition()
+    html = "<html><body>%s</ol></body></html>" % resp
     return HttpResponse(html)
