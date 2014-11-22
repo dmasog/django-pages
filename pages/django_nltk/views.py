8cf47317fa9931bd6a4fb8e47fa666310f72531b
Dave Masog <dave@masog.com>
2014-11-22 22:46:40 +0000

    updates to nltk

--- pages/django_nltk/views.py
+++ pages/django_nltk/views.py
@@ -1,13 +1,41 @@
 from django.http import HttpResponse
+from encodings import *
+from unicodedata import *
 import datetime
 import nltk
 from nltk.corpus import wordnet as wn
 import enchant
 
+import subprocess
+
+def pronunciation(p):
+   ret = []
+   if p == "":
+      p = "dog"
+   r = subprocess.call(["curl","-o","out","www.thefreedictionary.com/"+p])
+
+   with open("out") as f:
+      ln=f.read()
+
+   while ln.find("Click for pronun") > 0:
+      ind= ln.find("Click for pronun")
+      ln = ln[ind+1:]
+      hit = ln[ln.find(">")+1:ln.find(">")+200:].strip()
+      if hit[0]=="(":
+         hit= hit[1:hit.find(",")]
+      else:
+         hit= hit[:hit.find("<")]
+      if hit[0]!="-":
+         ret.append(hit)
+   return ret
+
 def home(request,parm=""):
+    pronun = pronunciation(parm)
     now = datetime.datetime.now()
-    resp = "%s:<br><ol>" % parm 
+    resp = "%s     %s:<br><ol>" % (parm,pronun)
     for i in wn.synsets(parm):
         resp += "<li>%s</li>" % i.definition()
     html = "<html><body>%s</ol></body></html>" % resp
-    return HttpResponse(html)
+    print "html:",html
+    
+    return HttpResponse(html.encode("utf-16"))
